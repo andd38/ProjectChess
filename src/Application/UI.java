c9d326c7d24370205fee0bed176498b8697a28c3
andre patriarca <andrepatriarca4@gmail.com>
2023-11-23 15:04:31 -0300

    Erro consertado

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -15,7 +15,7 @@ public class UI {
 	}
 	private static void printPiece(ChessPiece piece) {
 		if(piece == null) {
-			System.out.println("-");
+			System.out.print("-");
 		} else {
 			System.out.println(piece);
 		}
