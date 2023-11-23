12c440a75b3f7d7e7081d87fbe5f810518f42ddb
andre patriarca <andrepatriarca4@gmail.com>
2023-11-23 14:46:52 -0300

    Erro ao criar a UI.java

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,11 +2,13 @@ package Application;
 
 import Boardgame.Board;
 import Boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 7);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
