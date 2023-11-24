6c5f3829edcb2818f15b5b6d4edccfa1ba225977
andre patriarca <andrepatriarca4@gmail.com>
2023-11-24 08:15:42 -0300

    Desenhando peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,13 +1,17 @@
 package chess;
 
 import Boardgame.Board;
+import Boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
 
 	public ChessMatch() {
-		super();
+		
 		this.board = new Board(8, 8);
+		initialSteup();
 	}
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][]mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -18,6 +22,10 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
+	private void initialSteup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 3));
+		board.placePiece(new King(board, Color.BLACK), new Position(2, 4));
+	}
 	
 }
